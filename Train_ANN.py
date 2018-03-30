d10fc48a148ddb358afe14f92f1c6b22d0045bb5
bryce0515 <bryce0515@gmail.com>
2018-03-30 09:55:39 -0400

    Update Train_ANN.py

--- Train_ANN.py
+++ Train_ANN.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Wed Jan 17 13:07:21 2018
 
@@ -7,22 +6,27 @@ Created on Wed Jan 17 13:07:21 2018
 
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 import glob
 import sys
 import keras
-import h5py
+#import h5py
+
 from keras.models import Sequential
-#from keras.layers import LSTM, Dense
-from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dense, Dropout
 from sklearn.model_selection import train_test_split
 
 print ('Loading training data...')
 e0 = cv2.getTickCount()
 
+num_epochs = 30
+batch_size = 10
+image_size = 54000
 # load training data
-image_array = np.zeros((1, 38400))
+image_array = np.zeros((1, image_size))
 label_array = np.zeros((1, 4), 'float')
-training_data = glob.glob('training_data/*.npz')
+training_data = glob.glob('training_data_ANN/*.npz')
 
 
 # if no data, exit
@@ -48,9 +52,10 @@ print ('Loading image duration:', time0)
 
 # train test split, 7:3
 # X_train, X_test, y_train, y_test
-train, test, train_labels, test_labels = train_test_split(X, y, test_size=0.2)
+train, test, train_labels, test_labels = train_test_split(X, y, test_size=0.3)
 
-# Feature Scaling
+
+## Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 train = sc.fit_transform(train)
@@ -62,24 +67,92 @@ e1 = cv2.getTickCount()
 # Initialising the ANN
 classifier = Sequential()
 
-# Adding the input layer and the first hidden layer
-classifier.add(Dense(units = 32, kernel_initializer = 'uniform', activation = 'relu', input_dim = 38400))
+#X = X.reshape((1,38400,len(image_array))
+# Adding the input layer 
+#classifier.add(LSTM(32, input_shape = (38400,image_array[0])))
+classifier.add(Dense(units = 32, kernel_initializer = 'uniform', activation = 'relu', input_dim = image_size))
+classifier.add(Dropout(0.5))
+
+# Adding the first hidden layer
+classifier.add(Dense(units = 32, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dropout(0.5))
 
 # Adding the second hidden layer
 classifier.add(Dense(units = 32, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dropout(0.5))
+
+# Adding the third hidden layer
+classifier.add(Dense(units = 32, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dropout(0.5))
 
 # Adding the output  layer, 
-classifier.add(Dense(units = 4, kernel_initializer = 'uniform', activation = 'sigmoid'))
+classifier.add(Dense(units = 4, kernel_initializer = 'uniform', activation = 'softmax'))
 
 # Compiling the ANN
-classifier.compile(loss=keras.losses.binary_crossentropy,
-                   optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
+#classifier.compile(loss=keras.losses.binary_crossentropy,
+#                   optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True),metrics=['accuracy'])
 # Compiling the ANN
-# classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Viewing model_configuration
+
+classifier.summary()
+classifier.get_config()
+classifier.layers[0].get_config()
+classifier.layers[0].input_shape
+classifier.layers[0].output_shape
+classifier.layers[0].get_weights()
+np.shape(classifier.layers[0].get_weights()[0])
+classifier.layers[0].trainable
 
 # Fitting the ANN to the Training set
-classifier.fit(train, train_labels, batch_size = 10, epochs = 100)
+#hist = classifier.fit(train, train_labels, batch_size = 16, epochs = num_epochs, validation_data=(test, test_labels))
 
+# Training with callbacks
+from keras import callbacks
+
+filename='model_train_new.csv'
+csv_log=callbacks.CSVLogger(filename, separator=',', append=False)
+
+#early_stopping=callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='min')
+
+filepath="Best-weights-my_model-{epoch:03d}-{loss:.4f}-{acc:.4f}.hdf5"
+
+checkpoint = callbacks.ModelCheckpoint(filepath, monitor='val_loss',verbose=1, save_best_only=True, mode='min')
+
+callbacks_list = [csv_log,checkpoint]
+
+hist = classifier.fit(train, train_labels, batch_size = batch_size,verbose=1, epochs = num_epochs, validation_data=(test, test_labels),callbacks=callbacks_list)
+
+
+# visualizing losses and accuracy
+train_loss=hist.history['loss']
+val_loss=hist.history['val_loss']
+train_acc=hist.history['acc']
+val_acc=hist.history['val_acc']
+xc=range(num_epochs)
+
+plt.figure(1,figsize=(7,5))
+plt.plot(xc,train_loss)
+plt.plot(xc,val_loss)
+plt.xlabel('num of Epochs')
+plt.ylabel('loss')
+plt.title('train_loss vs val_loss')
+plt.grid(True)
+plt.legend(['train','val'])
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+plt.figure(2,figsize=(7,5))
+plt.plot(xc,train_acc)
+plt.plot(xc,val_acc)
+plt.xlabel('num of Epochs')
+plt.ylabel('accuracy')
+plt.title('train_acc vs val_acc')
+plt.grid(True)
+plt.legend(['train','val'],loc=4)
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
 
 # set end time
 e2 = cv2.getTickCount()
@@ -94,5 +167,6 @@ y_pred = (y_pred > 0.5)
 test_rate = np.mean(y_pred == test_labels)
 print ('Test accuracy: ', "{0:.2f}%".format(test_rate * 100))
 
+
 # save model
-classifier.save_weights('Saved_Weights_ANN.h5')
+classifier.save('Saved_Weights.xml')
